34d49c58984f1652d085bd0bf768beafdb0efcae
Derrick Otte <derrickotte@gmail.com>
2021-02-14 17:28:24 -0500

    Phase 2 Design of Splash Page, About section and Services section

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -2,17 +2,21 @@ function Landing() {
   return (
     <section className="landing">
       <div className="landing-text">
-        <div className="text">
+        <h1>Hair Salon and Spa for Men</h1>
+        <p>Fusce enim purus, dapibus eu porta vel, semper id est. 
+          Nunc sem odio, placerat ut finibus et, auctor sed tortor. 
+          Mauris dictum mattis est, vel tincidunt odio sagittis ut.</p>
+        <button>Book an Appointment</button>
+      </div>
+
+      {/* <div className="demo-text">
           <h1>Hair Salon and Spa for Men</h1>
-          <p>Fusce enim purus, dapibus eu porta vel, semper id est. 
-            Nunc sem odio, placerat ut finibus et, auctor sed tortor. 
+          <p>Fusce enim purus, dapibus eu porta vel, semper id est.
+          Nunc sem odio, placerat ut finibus et, auctor sed tortor.
             Mauris dictum mattis est, vel tincidunt odio sagittis ut.</p>
           <button>Book an Appointment</button>
-        </div>
-      </div>
-      <div className="landing-image">
-        <div className="shadow"></div>
-      </div>
+      </div> */}
+      {/* <div className="landing-image"></div> */}
     </section>
   )
 }
